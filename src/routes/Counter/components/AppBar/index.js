f53c70e976b7f13df099241faa273879c6b98bc3
Alex Vasylenko <alex.vasylenko@dev-pro.net>
2017-06-02 15:40:05 +0300

    1h - Add modal and snack bar in home page

--- src/routes/Counter/components/AppBar/index.js
+++ src/routes/Counter/components/AppBar/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import IconButton from 'material-ui/IconButton'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import classNames from 'classnames'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import Snackbar from 'material-ui/Snackbar'
 
 import ContentAdd from 'material-ui/svg-icons/content/add'
 import EditIcon from 'material-ui/svg-icons/editor/mode-edit'
@@ -10,26 +13,52 @@ import Search from 'material-ui/svg-icons/action/search'
 import Delete from 'material-ui/svg-icons/action/delete'
 import Close from 'material-ui/svg-icons/navigation/close'
 
-import styles from './styles.scss'
+const selectAppBar = (selected, selectClass, unselect, onClose, onOpen, isOpen, onDelete) => {
+  const actions = [
+    <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={onClose}
+    />,
+    <FlatButton
+        label="Delete"
+        primary={true}
+        onTouchTap={onDelete}
+    />,
+  ]
+  return (
+      <div className={selectClass}>
+        <Dialog
+            actions={actions}
+            modal={false}
+            open={isOpen}
+            onRequestClose={onClose}
+        >
+          Delete selected tasks?
+        </Dialog>
+        <nav className='selection-nav edit-mode'>
+          <IconButton onClick={unselect}>
+            <Close />
+          </IconButton>
+          { selected ? <span className='title'>{selected} selected</span> : ''}
+          { selected === 1 ? <IconButton ><EditIcon /></IconButton> : '' }
+          <IconButton onClick={onOpen}>
+            <Delete />
+          </IconButton>
+        </nav>
+      </div>
+  )
+}
 
-
-const selectAppBar = (selected, selectClass, unselect) => (
-    <div className={selectClass}>
-      <nav className='selection-nav edit-mode'>
-        <IconButton onClick={unselect}>
-          <Close />
-        </IconButton>
-        { selected ? <span className='title'>{selected} selected</span> : ''}
-        { selected === 1 ? <IconButton ><EditIcon /></IconButton> : '' }
-        <IconButton>
-          <Delete />
-        </IconButton>
-      </nav>
-    </div>
-)
-
-const unselectAppBar = (selected) => (
+const unselectAppBar = (snack, closeSnack) => (
     <div className='wrapper'>
+      <Snackbar
+          open={snack.open}
+          message={snack.message}
+          action="undo"
+          autoHideDuration={3000}
+          onRequestClose={closeSnack}
+      />
       <nav className="app-bar">
         <FloatingActionButton className='fab'>
           <ContentAdd />
@@ -38,29 +67,76 @@ const unselectAppBar = (selected) => (
         <IconButton className=' search '>
           <Search />
         </IconButton>
-        <div className='inputContainer '>
-          <input placeholder='Search ' />
-          <md-icon>clear</md-icon>
-        </div>
       </nav>
     </div>
 )
 
 
-export const AppBar = ({ selected, isMobile, unselect }) => {
-  const selectClass = classNames({
-    wrapper: true,
-    'selection-mode': selected
-  });
-
-  if (selected) {
-    return selectAppBar(selected, selectClass, unselect);
+export class AppBar extends React.Component {
+  static propTypes = {
+    selected: PropTypes.Array,
+    isMobile: PropTypes.bool,
   }
-  return unselectAppBar();
+
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false,
+      snack: {
+        message: 'Task deleted',
+        open: false,
+      }
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({isOpen: true});
+  };
+
+  handleClose = () => {
+    this.setState({isOpen: false});
+  };
+
+  handleDelete = () => {
+    this.setState({
+      isOpen: false,
+    });
+    this.props.unselect();
+    this.snackOpen();
+  };
+
+  snackClose = () => {
+    this.setState({
+      snack: {
+          ...this.state.snack,
+         open: false
+      }
+    })
+  }
+
+  snackOpen = () => {
+    this.setState({
+      snack: {
+          ...this.state.snack,
+        open: true
+      }
+    })
+  }
+
+  render() {
+    // refactor modal
+    const selected = this.props.selected;
+    const selectClass = classNames({
+      wrapper: true,
+      'selection-mode': selected
+    });
+
+    if (selected) {
+      return selectAppBar(selected, selectClass, this.props.unselect, this.handleClose, this.handleOpen, this.state.isOpen, this.handleDelete);
+    }
+    return unselectAppBar(this.state.snack, this.snackClose);
+  }
 }
-AppBar.propTypes = {
-  selected: PropTypes.Array,
-  isMobile: PropTypes.bool,
-}
+
 
 export default AppBar
